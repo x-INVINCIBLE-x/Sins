bf0d0b5c8a48c45afc8c749206e66819445a3085
Shubham <shubham170905@gmail.com>
2024-07-05 23:36:39 +0530

    Setup New Input system

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputManager : PlayerControls.IPlayerActions
+{
+    public void OnMovement(InputAction.CallbackContext context)
+    {
+        throw new System.NotImplementedException();
+    }
+}
